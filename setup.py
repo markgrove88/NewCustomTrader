3ecb16990b83a18556d4772679c347e2ae966970
markgrove88 <66644367+markgrove88@users.noreply.github.com>
2021-04-20 19:31:27 +0100

    Update Setup Params

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 from setuptools import setup
 
-setup(name='newCustomTrader',
+setup(
+      name='newCustomTrader',
       version='0.0.1',
-      install_requires=['gym', 'stable-baselines', 'numpy', 'pandas', 'matplotlib', 'yfinance', 'finta']  # And any other dependencies foo needs
+      install_requires=['gym', 'numpy', 'pandas', 'matplotlib'],
+      description='',
+      url='',
+      author='Mark Grove',
+      author_email='',
+      license='unlicense',
+      packages=['newCustomTrader'],
+      zip_safe=False
 )
