a4c34dfff702d3185b0feddecd94f45eb92f39c7
Mark Grove <markgrove@Marks-MacBook-Pro.local>
2021-04-20 11:57:35 +0100

    Changed Setup.py File

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='New_CustomTrader',
+setup(name='newCustomTrader',
       version='0.0.1',
       install_requires=['gym', 'stable-baselines', 'numpy', 'pandas', 'matplotlib', 'yfinance', 'finta']  # And any other dependencies foo needs
 )
