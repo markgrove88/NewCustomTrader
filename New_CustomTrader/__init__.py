87f4f3e6052e78cf26fcae4429274fb2424707d1
Mark Grove <markgrove@Marks-MacBook-Pro.local>
2021-04-20 11:05:11 +0100

    Altered path in init.py

--- New_CustomTrader/__init__.py
+++ New_CustomTrader/__init__.py
@@ -2,9 +2,9 @@ from gym.envs.registration import register
 
 register(
     id='cryptotrade-v0',
-    entry_point='gym_foo.envs:newCryptoEnv',
+    entry_point='New_CustomTrader.envs:newCryptoEnv',
     kwargs={
         'window_size': 24,
-        'frame_bound': (24, len(datasets.FOREX_EURUSD_1H_ASK))
+        'frame_bound': (24, 250)
     }
 )
