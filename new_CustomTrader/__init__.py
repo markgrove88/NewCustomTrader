efd7289ec4b72e5724ff208839a25d68965d038e
markgrove88 <66644367+markgrove88@users.noreply.github.com>
2021-04-21 23:26:47 +0100

    Changed entry point to CryptoEnv

--- new_CustomTrader/__init__.py
+++ new_CustomTrader/__init__.py
@@ -2,7 +2,7 @@ from gym.envs.registration import register
 
 register(
     id='cryptotrade-v0',
-    entry_point='new_CustomTrader.envs:CryptoEnvironment',
+    entry_point='new_CustomTrader.envs:CryptoEnv',
     kwargs={
         'window_size': 24,
         'frame_bound': (24, 250)
