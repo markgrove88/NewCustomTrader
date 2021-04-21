3ff872a2f9c197e88c064c6c50d68f7828712b5a
markgrove88 <66644367+markgrove88@users.noreply.github.com>
2021-04-21 23:28:53 +0100

    Update __init__.py

--- new_CustomTrader/envs/__init__.py
+++ new_CustomTrader/envs/__init__.py
@@ -1,1 +1,4 @@
-from .newCryptoEnv import CryptoEnvironment, Actions, Positions
+#from .newCryptoEnv import CryptoEnvironment, Actions, Positions
+
+from .trading_env import TradingEnv, Actions, Positions
+from .crypto_env import CryptoEnv
