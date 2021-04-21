ba5260b25968c32fb20430a8230f9c0093bc3aeb
markgrove88 <66644367+markgrove88@users.noreply.github.com>
2021-04-21 11:41:51 +0100

    Changed Version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
       name='newCustomTrader',
-      version='0.0.3',
+      version='0.0.4',
       install_requires=['gym', 'numpy', 'pandas', 'matplotlib'],
       description='',
       url='',
